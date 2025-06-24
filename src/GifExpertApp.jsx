9bf33275e89b58f6df9591bc7959aed145773f3b
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-06-24 12:35:31 -0500

    list Components

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+
+    const [categories, setCategories]  = useState(['One Punch', 'DINOSAURIOS', 'LOL']);
+
+    const onAddCategory = () => {
+
+      // console.log('Valorant');
+      setCategories([...categories, 'Valorant']);
+      // setCategories( cat => [...cat, 'Valorant'])  // al final
+      // setCategories( cat => ['Valorant', ...cat ])  // al princio
+    }
+
+  console.log(categories)
   return (
 
         <>
@@ -10,6 +24,15 @@ export const GifExpertApp = () => {
 
 
             {/* list of gif  */}
+            <button  onClick={ onAddCategory }>Agregar</button>
+            <ol>
+              { categories.map( category => {
+                  return  <li key={ category }> { category } </li>
+              } )  
+            
+              }
+            </ol>
+
             {/* gif items */}
 
 
