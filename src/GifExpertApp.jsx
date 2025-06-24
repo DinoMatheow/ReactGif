6b8fb41ea67eb4222609c8848155be5f6cc2162a
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-06-24 10:47:22 -0500

    initial commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,20 @@
+
+export const GifExpertApp = () => {
+  return (
+
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* input  */}
+
+
+            {/* list of gif  */}
+            {/* gif items */}
+
+
+
+        </>
+
+  )
+}
